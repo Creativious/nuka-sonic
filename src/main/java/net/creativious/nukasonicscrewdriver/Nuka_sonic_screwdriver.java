ad86985d12039209238ccb596fcda28f7d458803
Creativious <timothyu120@gmail.com>
2023-04-04 13:36:50 -0400

    Initial Project Setup

---
+++ src/main/java/net/creativious/nukasonicscrewdriver/Nuka_sonic_screwdriver.java
@@ -0,0 +1,10 @@
+package net.creativious.nukasonicscrewdriver;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Nuka_sonic_screwdriver implements ModInitializer {
+    @Override
+    public void onInitialize() {
+
+    }
+}
---
+++ src/main/java/net/creativious/nukasonicscrewdriver/client/Nuka_sonic_screwdriverClient.java
@@ -0,0 +1,10 @@
+package net.creativious.nukasonicscrewdriver.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class Nuka_sonic_screwdriverClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
